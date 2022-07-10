7f0e22c177a55140d5521de1651f2af31d70714e
t0mmy <tommycsth61@gmail.com>
2022-07-10 20:45:50 +0900

    ectoのセットアップ

--- sample/lib/sample/sample.ex
+++ sample/lib/sample/sample.ex
@@ -3,7 +3,9 @@ defmodule Sample.Application do
 
   @impl true
   def start(_type, _args) do
-    children = []
+    children = [
+      Sample.Repo,
+    ]
 
     opts = [strategy: :one_for_one, name: Hogee.Supervisor]
     Supervisor.start_link(children, opts)
